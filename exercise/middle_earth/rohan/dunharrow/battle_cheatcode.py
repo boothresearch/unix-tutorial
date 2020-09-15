fcc74377aeb421158af38fde6522aaacaefef5ca
samwallachhanson <68563943+samwallachhanson@users.noreply.github.com>
2020-09-14 21:12:31 -0400

    Update battle_cheatcode.py

--- exercise/middle_earth/rohan/dunharrow/battle_cheatcode.py
+++ exercise/middle_earth/rohan/dunharrow/battle_cheatcode.py
@@ -1,14 +1,20 @@
-
-from subprocess import call
-
+import os
 
 def easy_win(): 
     
+    try:
+        os.remove('/workspace/unix-tutorial/exercise/middle_earth/gondor/minas_tirith/ghost_army.txt')
+    except:
+        pass
+    
+
     f= open("/workspace/unix-tutorial/exercise/middle_earth/gondor/minas_tirith/ghost_army.txt","w+")
     f.write("Spooooooky ghooooooosts")
     f.close()
-
-    call('rm /workspace/unix-tutorial/exercise/middle_earth/gondor/minas_tirith/orcs.txt')
+    try:
+        os.remove('/workspace/unix-tutorial/exercise/middle_earth/gondor/minas_tirith/orcs.txt')
+    except:
+        pass
 
 def main():
     print("Aragorn: Fight for me and I will hold your oaths fulfilled!")
